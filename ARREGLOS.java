e13f12c15c293ef11b402efa8bfc49b63f5dbf22
PINKPONYPUNK <nathalia.jannet.suarez@gmail.com>
2025-08-26 11:41:26 -0600

    Add files via upload

---
+++ ARREGLOS.java
@@ -0,0 +1,27 @@
+package practica1
+public class ARREGLOS {
+import java.util.*;
+public static void main (String [] args) {
+	int[] numeros = new int[15];
+	int num;
+	Scanner captura = new Scanner (System.in);
+	for(int x = 0; x < numeros.length; x++) {
+	System.out.println("dame el valor:" +x);
+	num = captura.nextInt();
+	numeros[x] = num;
+}
+	for(int i = 0; i <numeros.length; i ++) {
+	//string n = numero [i] %2 == 0? "hola" : "no"	
+		if(numeros[i] %2 == 0) {
+			double raiz;
+			raiz =Math.sqrt(numeros[i] > 0) {
+			System.out.println("la raiz cuadradade" + numero[x]+" " raiz);
+		if(numeros[i] < 0) {
+			
+			contador_neg += 1;
+		}
+		}
+	}
+}
+}
+}
